6124195dfcadf5a365cde856e9fe741e27676713
Scaria <jscaria@osigroup.com>
2018-11-09 15:28:39 -0600

    update - another test with react - charlieReact

---
+++ charlieReact/precache-manifest.505148c96b16c48adac6ea3875ad822d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "141b5f0cb6426b1f7169",
+    "url": "/charlieReact/static/js/runtime~main.141b5f0c.js"
+  },
+  {
+    "revision": "472d4e43c353d002131f",
+    "url": "/charlieReact/static/js/main.472d4e43.chunk.js"
+  },
+  {
+    "revision": "d390a7e15b6114422e4c",
+    "url": "/charlieReact/static/js/1.d390a7e1.chunk.js"
+  },
+  {
+    "revision": "472d4e43c353d002131f",
+    "url": "/charlieReact/static/css/main.396148f8.chunk.css"
+  },
+  {
+    "revision": "ade557251bab33f775ede1bb83b7de5e",
+    "url": "/charlieReact/index.html"
+  }
+];
