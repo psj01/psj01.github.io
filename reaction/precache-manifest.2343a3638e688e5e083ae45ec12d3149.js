13356490301ead7e17b17cadc48b150c11e21117
Scaria <jscaria@osigroup.com>
2018-11-08 23:59:08 -0600

    update

---
+++ reaction/precache-manifest.2343a3638e688e5e083ae45ec12d3149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a96e60f9b22beda24139",
+    "url": "/reaction/static/js/runtime~main.a96e60f9.js"
+  },
+  {
+    "revision": "6a936ba59ac3e82306b8",
+    "url": "/reaction/static/js/main.6a936ba5.chunk.js"
+  },
+  {
+    "revision": "a0627d828396f7f6f007",
+    "url": "/reaction/static/js/1.a0627d82.chunk.js"
+  },
+  {
+    "revision": "6a936ba59ac3e82306b8",
+    "url": "/reaction/static/css/main.76f6b3db.chunk.css"
+  },
+  {
+    "revision": "27f29561424364314b6b17ebfd95dbdd",
+    "url": "/reaction/index.html"
+  }
+];
--- reaction/service-worker.js
+++ reaction/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44a09023ea26f38919e2aa2302899850.js"
+  "/reaction/precache-manifest.2343a3638e688e5e083ae45ec12d3149.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reaction/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
