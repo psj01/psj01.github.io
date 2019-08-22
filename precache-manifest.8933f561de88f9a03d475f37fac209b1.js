646339bfa428fdf6ff3f8e52e5af3da623ec2742
psj01 <z1262764@students.niu.edu>
2019-08-21 21:51:00 -0500

    updated personal site with react

---
+++ precache-manifest.8933f561de88f9a03d475f37fac209b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55ced71c7155784000511dd37c2a33d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff4556eb5bb5a76766e3",
+    "url": "/static/css/main.4b1846af.chunk.css"
+  },
+  {
+    "revision": "1fd426e0e9c736c04a84",
+    "url": "/static/js/2.4425f7c1.chunk.js"
+  },
+  {
+    "revision": "ff4556eb5bb5a76766e3",
+    "url": "/static/js/main.e96d1cb3.chunk.js"
+  },
+  {
+    "revision": "158456b140964882cd91",
+    "url": "/static/js/runtime~main.40f2c2dc.js"
+  },
+  {
+    "revision": "6b29f2b57edcd1159b8ae2db51aacb82",
+    "url": "/static/media/jobinPic.6b29f2b5.jpg"
+  }
+]);
